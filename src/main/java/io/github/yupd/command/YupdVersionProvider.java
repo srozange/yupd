4eb4bcea47eca4d9241ee7694351401ae4bdc5ce
Sebastien Rozange <srozange@gmail.com>
2023-08-26 08:21:30 +0200

    Display SHA-1 hash in --version command

--- src/main/java/io/github/yupd/command/YupdVersionProvider.java
+++ src/main/java/io/github/yupd/command/YupdVersionProvider.java
@@ -4,12 +4,26 @@ import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import picocli.CommandLine;
 
+import java.util.Optional;
+
+import static io.github.yupd.infrastructure.utils.StringUtils.isNotEmpty;
+
 public class YupdVersionProvider implements CommandLine.IVersionProvider {
 
     @Override
     public String[] getVersion() {
-        Config cf = ConfigProvider.getConfig(getClass().getClassLoader());
-        return new String[] { cf.getValue("quarkus.application.version", String.class) };
+        String appVersion = getPropertyValue("quarkus.application.version");
+        String commitVersion = getPropertyValue("commit.version");
+        return new String[] { "v" + appVersion + (isNotEmpty(commitVersion) ? " (" + commitVersion + ")" : "") };
+    }
+
+    private String getPropertyValue(String property) {
+        var config = ConfigProvider.getConfig(getClass().getClassLoader());
+        try {
+            return config.getValue(property, String.class);
+        } catch (java.util.NoSuchElementException e) {
+            return null;
+        }
     }
 
 }
